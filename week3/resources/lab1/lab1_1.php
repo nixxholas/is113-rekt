717128bf1fd10d31b59de35de09d1f477920ce06
nixxholas <nixholas@icloud.com>
2021-01-28 08:56:02 +0800

    week3: resources: lab1: q1: done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week3/resources/lab1/lab1_1.php
+++ week3/resources/lab1/lab1_1.php
@@ -1,5 +1,9 @@
 <?php
     # Write code here
+    DEFINE("π", 3.14);
 
+    $radius = 4;
+    echo "Area of circle of radius " . $radius . " cm is " . (π * ($radius * $radius)) . " sq cm </br>";
+    echo "Circumference of circle of radius " . $radius . " cm is " . (π * ($radius * 2)) . "  cm";
     # End of code
 ?>
