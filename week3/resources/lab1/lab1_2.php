904597a367c270759d0190d876768f1f04256cef
nixxholas <nixholas@icloud.com>
2021-01-28 09:01:21 +0800

    week3: resources: lab1: q2: Done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week3/resources/lab1/lab1_2.php
+++ week3/resources/lab1/lab1_2.php
@@ -1,6 +1,14 @@
 <?php
 $var = 60;
 # Write code here
+DEFINE("EARTH_MULTIPLIER", 1);
+DEFINE("MERCURY_MULTIPLIER", 0.4);
+DEFINE("VENUS_MULTIPLIER", 0.9);
+DEFINE("JUPITER_MULTIPLIER", 2.5);
 
+echo "Your weight on Earth" . " is " . ($var * EARTH_MULTIPLIER) . " kg <br/>";
+echo "Your weight on Mercury" . " is " . ($var * MERCURY_MULTIPLIER) . " kg <br/>";
+echo "Your weight on Venus" . " is " . ($var * VENUS_MULTIPLIER) . " kg <br/>";
+echo "Your weight on Jupiter" . " is " . ($var * JUPITER_MULTIPLIER) . " kg <br/>";
 # End of code
 ?>
