d5aa348903516d7689636735a6b8229a8935d6da
nixxholas <nixholas@icloud.com>
2021-02-04 10:12:34 +0800

    week4: ex9: process_form: done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week4/ex9/process_form.php
+++ week4/ex9/process_form.php
@@ -9,9 +9,12 @@
             // 1. Complete the codes below to print out the topics selected
             // 2. Include validation if needed
 
-            
+            $form_data = $_GET["topics"];
 
-            
+            if ($form_data != null && sizeof($form_data) > 0)
+                foreach ($form_data as $topic) {
+                    echo "$topic <br>";
+                }
         ?>
 
         </ul>
