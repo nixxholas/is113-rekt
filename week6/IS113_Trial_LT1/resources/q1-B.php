b04797f3e442eb72171faece997e8db451b06082
nixxholas <nixholas@icloud.com>
2021-02-18 10:16:44 +0800

    week6: IS113_Trial_LT1: q1b done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week6/IS113_Trial_LT1/resources/q1-B.php
+++ week6/IS113_Trial_LT1/resources/q1-B.php
@@ -3,4 +3,17 @@
     Name:  
     Email: 
 */
+    function is_divisible_by($num, $n) {
+        return (($num % $n == 0) == 1) ? 'YES' : 'NO';
+    }
+
+    if (isset($_GET["numbers"]) && isset($_GET["divisor"])) {
+        $divisor = $_GET["divisor"];
+        $numbers = explode(',', $_GET["numbers"]);
+
+        foreach ($numbers as $number) {
+            $numDivisible = is_divisible_by($number, $divisor);
+            echo "<li>$number is divisible by $divisor: $numDivisible </li>";
+        }
+    }
 ?>
