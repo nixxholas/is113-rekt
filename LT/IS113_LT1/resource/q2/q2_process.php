e9f28f5fe8316f3169d81082f2d600cf28b8b39c
nixxholas <nixholas@icloud.com>
2021-03-02 14:25:05 +0800

    LT1: q2: Done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- LT/IS113_LT1/resource/q2/q2_process.php
+++ LT/IS113_LT1/resource/q2/q2_process.php
@@ -18,4 +18,92 @@ $warranty_info =   array(
                     );
 
 
+$upg1 = isset($_POST["upg1"]);
+$upg2 = isset($_POST["upg2"]);
+
+if ($upg1 && $upg2) {
+    echo "You can only select one upgrade option from 1-2";
+} else {
+    $upg3 = isset($_POST["upg3"]);
+    $warranty = intval($_POST["warranty"]);
+//    echo $warranty;
+
+    echo "<h1>Please confirm your selection</h1>";
+    echo "<table border='1'>";
+    echo "<thead>";
+    echo "<th>No.</th>";
+    echo "<th>Description</th>";
+    echo "<th>Price w/o GST</th>";
+    echo "</thead>";
+
+    $rowCount = 1;
+
+    echo "<tr>";
+    echo "<td>$rowCount</td>";
+    echo "<td>ThinkPad X1 Carbon Gen 8</td>";
+    echo "<td>$1,669.00</td>";
+    echo "</tr>";
+
+    $rowCount += 1;
+
+    $subtotal = 1669.00;
+
+    if ($upg1) {
+        $upg1val = intval($_POST["upg1"]);
+        echo "<tr>";
+        echo "<td>$rowCount</td>";
+        echo "<td>" . $upgrade_info[$upg1val][0] . "</td>";
+        echo "<td>$" . number_format(doubleval($upgrade_info[$upg1val][1]), 2) . "</td>";
+        echo "</tr>";
+        $rowCount += 1;
+        $subtotal += doubleval($upgrade_info[$upg1val][1]);
+    }
+
+    if ($upg2) {
+        $upg2val = intval($_POST["upg2"]);
+        echo "<tr>";
+        echo "<td>$rowCount</td>";
+        echo "<td>" . $upgrade_info[$upg2val][0] . "</td>";
+        echo "<td>$" . number_format(doubleval($upgrade_info[$upg2val][1]), 2) . "</td>";
+        echo "</tr>";
+        $rowCount += 1;
+        $subtotal += doubleval($upgrade_info[$upg2val][1]);
+    }
+
+    if ($upg3) {
+        $upg3val = intval($_POST["upg3"]);
+        echo "<tr>";
+        echo "<td>$rowCount</td>";
+        echo "<td>" . $upgrade_info[$upg3val][0] . "</td>";
+        echo "<td>$" . number_format(doubleval($upgrade_info[$upg3val][1]), 2) . "</td>";
+        echo "</tr>";
+        $rowCount += 1;
+        $subtotal += doubleval($upgrade_info[$upg3val][1]);
+    }
+
+    echo "<tr>";
+    echo "<td>$rowCount</td>";
+    echo "<td>" . $warranty_info[$warranty][0] . "</td>";
+    echo "<td>$" . number_format(doubleval($warranty_info[$warranty][1]), 2) . "</td>";
+    echo "</tr>";
+    $subtotal += doubleval($warranty_info[$warranty][1]);
+
+    echo "<tr>";
+    echo "<td colspan='2'>Subtotal</td>";
+    echo "<td>$" . number_format($subtotal, 2) . "</td>";
+    echo "</tr>";
+
+    $gst = (doubleval($subtotal) / 100) * 7;
+    echo "<tr>";
+    echo "<td colspan='2'>GST</td>";
+    echo "<td>$" . number_format($gst, 2) ."</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td colspan='2'>Total</td>";
+    echo "<td>$" . number_format($subtotal + $gst, 2) . "</td>";
+    echo "</tr>";
+
+    echo "</table>";
+}
 ?>
