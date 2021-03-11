a4991360a796becc982aae767998e1454907e147
nixxholas <nixholas@icloud.com>
2021-03-11 09:10:43 +0800

    week9: house: display_2a: Initial completion?
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week9/house/display_2a.php
+++ week9/house/display_2a.php
@@ -1,37 +1,38 @@
 <?php
 
-    // 0. Import the class we need
-    require_once "House.php";
+// 0. Import the class we need
+require_once "House.php";
 
-    // 1. These are the values
-    $new_houses = [  ["HDB", 4, "1010", "Montreal Drive"],
-                     ["HDB", 3, "870", "King George Avenue"],
-                     ["CONDO", 2, "590", "East Coast"],
-                     ["CONDO", 5, "1400", "Yishun Park"],
-                     ["LANDED", 4, "2000", "University Road"]];
-   
+// 1. These are the values
+$new_houses = [["HDB", 4, "1010", "Montreal Drive"],
+    ["HDB", 3, "870", "King George Avenue"],
+    ["CONDO", 2, "590", "East Coast"],
+    ["CONDO", 5, "1400", "Yishun Park"],
+    ["LANDED", 4, "2000", "University Road"]];
 
-    // 2. Create an array of Objects
-    // Complete the codes
-    $new_houses_arr=[];
-  
-   
 
-    // Display the houses (before)
-    
+// 2. Create an array of Objects
+// Complete the codes
+$new_houses_arr = [];
 
-    // 2. Set prices
-   
+foreach ($new_houses as $flat) {
+    array_push($new_houses_arr, new House($flat[0], $flat[1], $flat[2], $flat[3]));
+}
 
-     // Display the houses (after)
-    
+display_houses($new_houses_arr);
+
+// 2. Set prices
+
+
+// Display the houses (after)
 
 
 #
 # This function displays the result of an indexed array of House objects
 # in a table format.
 #
-function display_houses ($house_arr){
+function display_houses($house_arr)
+{
 
     echo "<table border='1'> <tr> 
             <th> Type </th>
@@ -41,14 +42,14 @@ function display_houses ($house_arr){
             <th> Price </th>
             <th> Cost Sq Feet </th>
             </tr>";
-   
-    foreach ($house_arr as $h){
+
+    foreach ($house_arr as $h) {
         $type = $h->getType();
         $bedroom = $h->getBedroom();
         $size = $h->getSize();
         $location = $h->getLocation();
-        $price = number_format($h->getPrice(),0);
-        $cost_per_sq_feet = number_format($h->CostPerSqFeet(),2);
+        $price = number_format($h->getPrice(), 0);
+        $cost_per_sq_feet = number_format($h->CostPerSqFeet(), 2);
         echo "<tr>
                 <td>$type</td>
                 <td>$bedroom</td>
@@ -63,7 +64,6 @@ function display_houses ($house_arr){
     echo "<br> <br> <br>";
 
 }
- 
 
 
 ?>
