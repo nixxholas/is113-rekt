db21276cee9367bec9192a978cbf7efddc5c47db
nixxholas <nixholas@icloud.com>
2021-03-25 09:05:35 +0800

    week11: InClass: Revision: q2: display2: Done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week11/InClass/Revision/q2/display2.php
+++ week11/InClass/Revision/q2/display2.php
@@ -21,14 +21,28 @@
         //var_dump($_GET);
 
         if (isset($_GET["find"])){
-            
-            // YOUR CODES GOES HERE
+            $age = $_GET["age"];
 
+            if ($age > 0) {
+                $toyDao = new ToyDAO();
 
+                $toyResult = [];
+                foreach ($toyDao->getAllToys() as $toy) {
+                    if ($toy->getForAge() <= $age)
+                        array_push($toyResult, $toy);
+                }
 
-
-
-
+                if (sizeof($toyResult) == 0) {
+                    echo "No toys for age $age.";
+                } else {
+                    echo "Toys suitable for age : $age";
+                    echo "<ul>";
+                    foreach ($toyResult as $toy) {
+                        echo "<li>{$toy->getType()}</li>";
+                    }
+                    echo "</ul>";
+                }
+            }
         }
 
     ?>
