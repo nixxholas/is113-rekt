f375368e1d7be9241b35293c89a455e2d996aade
nixxholas <nixholas@icloud.com>
2021-03-25 10:30:35 +0800

    week11: InClass: Flower: model: ConnectionManager: Proper implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week11/InClass/Flower/model/ConnectionManager.php
+++ week11/InClass/Flower/model/ConnectionManager.php
@@ -1,20 +1,26 @@
 <?php
 
 class ConnectionManager {
+    private PDO $pdo;
 
-  public function getConnection() {
-    $servername = 'localhost';
-    $dbname = 'week11flower';
-    $username = 'root';
-    $password = '';
-    $port = 3306;
-    $url  = "mysql:host=$servername;dbname=$dbname;port=$port";
+    public function __construct() {
+        $servername = 'localhost';
+        $dbname = 'week11shopping';
+        $username = 'root';
+        $password = '';
+        $port = 3306;
+        $url = "mysql:host=$servername;dbname=$dbname;port=$port";
+        $this->pdo = new PDO($url, $username, $password);
+    }
 
-    
+    public function __destruct() {
+        $this->pdo = null;
+    }
 
-    return new PDO($url, $username, $password);
-  }
-
+    public function getConnection()
+    {
+        return $this->pdo;
+    }
 }
 
 ?>
