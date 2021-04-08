c2dc0a3a29010323485597d383b995e559ca3737
nixxholas <nixholas@icloud.com>
2021-04-08 10:43:51 +0800

    week13: Trial: q1: Done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week13/TLT2-resources-A/resources/q1/statistics.php
+++ week13/TLT2-resources-A/resources/q1/statistics.php
@@ -16,8 +16,25 @@ require_once 'model/common.php';
 		<img src="images/sis.png">
 		<h1>Statistics</h1>
         <?php
+            $respDao = new ResponseDAO();
             # == Part C (Compute Statistics): ENTER CODE HERE == 
-                    
+            $responses = $respDao->retrieveAll();
+
+            echo "<table border='1'>";
+            echo "<tr><th># Responses</th><td>" . sizeof($responses) . "</td></tr>";
+            $twoHourCount = 0;
+            $fifteenWeekCount = 0;
+            foreach ($responses as $response) {
+                if ($response->getPreferredClassLength() == 2) {
+                    $twoHourCount += 1;
+                }
+                if ($response->getPreferredSemLength() == 15) {
+                    $fifteenWeekCount += 1;
+                }
+            }
+            echo "<tr><th># 2 Hours</th><td>$twoHourCount</td></tr>";
+            echo "<tr><th>% 15 Weeks</th><td>" . round($fifteenWeekCount/sizeof($responses) * 100, 2) .  "%</td></tr>";
+            echo "</table>";
             # ====
 		?>
 	</body>
