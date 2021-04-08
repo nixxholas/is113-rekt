9bc7fe70e3580a97533b99e6c0909641dd22864c
nixxholas <nixholas@icloud.com>
2021-04-08 08:51:19 +0800

    week13: revision: q2: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week13/Revision/q2/display.php
+++ week13/Revision/q2/display.php
@@ -1,35 +1,35 @@
 <?php
 
-    require_once "models/Cohort.php";
-    require_once "models/Student.php";
-    require_once "models/Course.php";
-   
-    $course1 = new Course("Programming", 80);
-    $course2 = new Course("Programming",90);
-    $course3 = new Course("Database",65);
+require_once "models/Cohort.php";
+require_once "models/Student.php";
+require_once "models/Course.php";
 
-    $mary = new Student("Mary",[$course1]);
-    $john = new Student("John",[$course2,$course3]);
-    
-    $cohort_2021 =  new Cohort("2021", [$mary,$john]);    
+$course1 = new Course("Programming", 80);
+$course2 = new Course("Programming", 90);
+$course3 = new Course("Database", 65);
 
-    //var_dump($cohort_2021->getStudents());
-    
+$mary = new Student("Mary", [$course1]);
+$john = new Student("John", [$course2, $course3]);
 
-    #display all
-    #complete the codes
-    echo "<table border=1>";
-    echo "<tr><th>Name</th><th>Subject</th></tr>";
-    foreach (   as $stud ){
-        echo "<tr>
-                <td>" .    . "</td>";
-        $str='';
-        foreach (   as $s){
-            $str .=   . "  " .    .  "<br>";
-        }
-        echo "<td> $str </td>";
-        echo "</tr>";
+$cohort_2021 = new Cohort("2021", [$mary, $john]);
+
+//var_dump($cohort_2021->getStudents());
+
+
+#display all
+#complete the codes
+echo "<table border=1>";
+echo "<tr><th>Name</th><th>Subject</th></tr>";
+foreach ($cohort_2021->getStudents() as $stud) {
+    echo "<tr>
+                <td>" . $stud->getName() . "</td>";
+    $str = '';
+    foreach ($stud->getCourses() as $s) {
+        $str .= $s->getCourseName() . "  " . $s->getScore() . "<br>";
     }
-    echo "</table>";
+    echo "<td> $str </td>";
+    echo "</tr>";
+}
+echo "</table>";
 
 ?>
