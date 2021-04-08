06290be99bd4d851901e6dbbed308c34d1688183
nixxholas <nixholas@icloud.com>
2021-04-08 11:30:07 +0800

    week13: trial test: initial implementation for final q2 part
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week13/TLT2-resources-A/resources/q2/updatePassword.php
+++ week13/TLT2-resources-A/resources/q2/updatePassword.php
@@ -10,25 +10,27 @@ require_once 'model/protect.php';
 
 # == Part C : ENTER CODE HERE == 
 
+echo "<form method='get'>";
+$empId = $_GET["empId"];
+echo "<input type='hidden' name='empId' value=$empId />";
+//echo "$empId";
 
+echo "Employee ID: $empId<br>";
 
+$empDao = new EmployeeDAO();
+$emp = $empDao->getEmployee((int) $empId);
+echo "Name: " . $emp->getName() . "</br>";
+echo "Current Password: " . $emp->getPassword() . "</br>";
+$newPwd = generateRandomPassword();
+echo "New Password: <input type='text' name='newPassword' value='$newPwd' /></br>";
+echo "<input type='submit' name='updatePassword' value='Update'/>";
+echo "</form>";
 
-
-
-
-
-
-
-
+if (isset($_GET["updatePassword"])) {
+    echo "Supposed to update";
+}
 function generateRandomPassword(){
-# == Part C : ENTER CODE HERE == 
-
-
-
-
-
-
-
-
+    # == Part C : ENTER CODE HERE ==
+    return password_hash((string) random_int(99999, 999999), PASSWORD_DEFAULT);
 }
 ?>
