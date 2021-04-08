d4b4d680e80de766683e865f3365d84ab6a2a5f4
nixxholas <nixholas@icloud.com>
2021-04-08 10:37:54 +0800

    week13: Trial: part b done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week13/TLT2-resources-A/resources/q1/process_survey.php
+++ week13/TLT2-resources-A/resources/q1/process_survey.php
@@ -26,19 +26,31 @@ require_once 'model/common.php';
                 exit;
             }
             #===
-            echo "You entered: </br>";
+            echo "You entered: </br></br>";
+
+            $name = '';
+            if (isset($_SESSION["name"])) {
+                $name = $_SESSION["name"];
+            }
+            $class_length = $_POST["class_length"];
+            $sem_length = $_POST["sem_length"];
 
             # == Part B (Display student name and preferences): ENTER CODE HERE ==
-            session_start();
-            echo "<table>";
-            echo "<tr></tr>";
-            echo "<tr></tr>";
-            echo "<tr></tr>";
-            echo "</table>"
+            echo "<table border='1'>";
+            echo "<tr><th>Name</th><td>$name</td></tr>";
+            echo "<tr><th>Class Length</th><td>$class_length</td></tr>";
+            echo "<tr><th>Semester Length</th><td>$sem_length</td></tr>";
+            echo "</table></br>";
             # ===
             
             # == Part B (Add a new response to the database and display status): ENTER CODE HERE ==
-            
+            $respDao = new ResponseDAO();
+            $addRes = $respDao->addResponse($name, $class_length, $sem_length);
+            if ($addRes) {
+                echo "<strong>Response saved successfully</strong>";
+            } else {
+                echo "<strong>Response was not saved successfully</strong>";
+            }
             # ====
   
         ?>
