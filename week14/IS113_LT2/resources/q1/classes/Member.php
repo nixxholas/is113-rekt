fee40b8f9acab78b626ef43a879a6df38625157c
nixxholas <nixholas@icloud.com>
2021-04-13 19:08:35 +0800

    week14: LT2: q1c: Done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week14/IS113_LT2/resources/q1/classes/Member.php
+++ week14/IS113_LT2/resources/q1/classes/Member.php
@@ -1,5 +1,19 @@
 <?php
     
     ### Add code here
-           
+    class Member {
+        private $name;
+
+        public function __construct($name) {
+            $this->name = $name;
+        }
+
+        public function getName() {
+            return $this->name;
+        }
+
+        public function setName($name) {
+            $this->name = $name;
+        }
+    }
 ?>
