25da6d222cf704b33f82775432a469e6f27216e0
nixxholas <nixholas@icloud.com>
2021-04-13 20:59:33 +0800

    week14: LT2: q3: done
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week14/IS113_LT2/resources/q3/show_availability.php
+++ week14/IS113_LT2/resources/q3/show_availability.php
@@ -3,8 +3,73 @@
     require_once 'common.php';
     
     function display_timetable($students){
-        # Add code here
-        # You are free to create helper functions        
+        $day_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+        $schedule = [9 => [[],[],[],[],[]], 10 => [[],[],[],[],[]], 11 => [[],[],[],[],[]],
+            12 => [[],[],[],[],[]], 13 => [[],[],[],[],[]], 14 => [[],[],[],[],[]], 15 => [[],[],[],[],[]],
+            16 => [[],[],[],[],[]]];
+
+        foreach ($schedule as $time => $days) {
+            for ($i = 0; $i < sizeof($days); $i++) {
+                $cur_day = $day_names[$i];
+
+                foreach ($students as $student) {
+                    $timetable = $student->getTimetable();
+                    foreach ($timetable as $appt) {
+                        if ($appt->getDayOfTheWeek() == $cur_day && ($appt->getStartTime() <= $time
+                                && $time <= $appt->getStartTime() + $appt->getDuration())) {
+                            array_push($schedule[$time][$i], $student->getName());
+                        }
+                    }
+                }
+            }
+        }
+
+        $day_rows = [0,0,0,0,0];
+        echo "<table border='1'>";
+        echo "<thead><th></th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th></thead>";
+        // Loop every time slot
+        foreach ($schedule as $time => $days) {
+            echo "<tr>";
+            $end_time = $time + 1;
+            echo "<th>$time-$end_time</th>";
+            // Loop every day
+            for ($i = 0; $i < sizeof($days); $i++) {
+                $day = $days[$i];
+                $day_row = $day_rows[$i];
+
+                if (($time - 8) > $day_row) {
+                    echo var_dump($day) . "<br><br>";
+                    $thickness = 1;
+
+                    // If someone is unavailable
+                    if (sizeof($day) > 0) {
+                        // Keep looping next timeslot to see if there's more unavailability.
+                        $new_time = $time + 1;
+                        while (true) {
+                            if (sizeof($schedule[$new_time][$i]) > 0) {
+                                $thickness += 1;
+                                foreach ($schedule[$new_time][$i] as $cmi) {
+                                    if (!array_search($cmi, $day)) {
+                                        array_push($unavailable, $cmi);
+                                    }
+                                }
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    echo "<td rowspan='$thickness'>";
+                    foreach ($day as $person) {
+                        echo $person;
+                    }
+                    echo "</td>";
+                    $day_row += $thickness;
+                }
+            }
+            echo "</tr>";
+        }
+        echo "<table>";
     }
 
     ### START OF DO NOT MODIFY ###
