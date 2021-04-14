c1b3b1415c6441b0e5d2cbee5bff6273712a1111
nixxholas <nixholas@icloud.com>
2021-04-14 11:01:06 +0800

    week14: LT2: q3b: Answer is correct, just too slow
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- week14/IS113_LT2/resources/q3/show_availability.php
+++ week14/IS113_LT2/resources/q3/show_availability.php
@@ -4,108 +4,119 @@
 -->
 <?php
 
-    require_once 'common.php';
-    
-    function display_timetable($students){
-        $day_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
-        $schedule = [9 => [[],[],[],[],[]], 10 => [[],[],[],[],[]], 11 => [[],[],[],[],[]],
-            12 => [[],[],[],[],[]], 13 => [[],[],[],[],[]], 14 => [[],[],[],[],[]], 15 => [[],[],[],[],[]],
-            16 => [[],[],[],[],[]]];
+require_once 'common.php';
 
-        foreach ($schedule as $time => $days) {
-            for ($i = 0; $i < sizeof($days); $i++) {
-                $cur_day = $day_names[$i];
+function display_timetable($students)
+{
+    $day_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+    $schedule = [9 => [[], [], [], [], []], 10 => [[], [], [], [], []], 11 => [[], [], [], [], []],
+        12 => [[], [], [], [], []], 13 => [[], [], [], [], []], 14 => [[], [], [], [], []], 15 => [[], [], [], [], []],
+        16 => [[], [], [], [], []]];
 
-                foreach ($students as $student) {
-                    $timetable = $student->getTimetable();
-                    foreach ($timetable as $appt) {
-                        if ($appt->getDayOfTheWeek() == $cur_day && ($appt->getStartTime() <= $time
-                                && $time <= $appt->getStartTime() + $appt->getDuration())) {
-                            array_push($schedule[$time][$i], $student->getName());
-                        }
+    // For every time with the work days
+    foreach ($schedule as $time => $days) {
+        // For every day in the specific time
+        for ($i = 0; $i < sizeof($days); $i++) {
+            $cur_day = $day_names[$i];
+            // Check if the student is busy in this specific time
+            foreach ($students as $student) {
+                $timetable = $student->getTimetable();
+                foreach ($timetable as $appt) {
+                    // If the appointment is the current day of the week
+                    // And if the appt start time is == current time or
+                    // if the start time is less the the current time but end time is less than or equal to the
+                    // current time
+                    if ($appt->getDayOfTheWeek() == $cur_day && ($appt->getStartTime() == $time
+                            || ($appt->getStartTime() < $time
+                                && $time <= $appt->getStartTime() + $appt->getDuration()))) {
+                        array_push($schedule[$time][$i], $student->getName());
                     }
                 }
             }
         }
-
-        $day_rows = [0,0,0,0,0];
-        echo "<table border='1'>";
-        echo "<thead><th></th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th></thead>";
-        // Loop every time slot
-        foreach ($schedule as $time => $days) {
-            echo "<tr>";
-            $end_time = $time + 1;
-            echo "<th>$time-$end_time</th>";
-            // Loop every day
-            for ($i = 0; $i < sizeof($days); $i++) {
-                $day = $days[$i];
-                $day_row = $day_rows[$i];
-
-                if (($time - 8) > $day_row) {
-                    echo var_dump($day) . "<br><br>";
-                    $thickness = 1;
-
-                    // If someone is unavailable
-                    if (sizeof($day) > 0) {
-                        // Keep looping next timeslot to see if there's more unavailability.
-                        $new_time = $time + 1;
-                        while (true) {
-                            if (sizeof($schedule[$new_time][$i]) > 0) {
-                                $thickness += 1;
-                                foreach ($schedule[$new_time][$i] as $cmi) {
-                                    if (!array_search($cmi, $day)) {
-                                        array_push($unavailable, $cmi);
-                                    }
-                                }
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-
-                    echo "<td rowspan='$thickness'>";
-                    foreach ($day as $person) {
-                        echo $person;
-                    }
-                    echo "</td>";
-                    $day_row += $thickness;
-                }
-            }
-            echo "</tr>";
-        }
-        echo "<table>";
     }
 
-    ### START OF DO NOT MODIFY ###
+    $day_rows = [8, 8, 8, 8, 8];
+    echo "<table border='1'>";
+    echo "<thead><th></th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th></thead>";
+    // Loop every time slot
+    foreach ($schedule as $time => $days) {
+        echo "<tr>";
+        $end_time = $time + 1;
+        echo "<th>$time-$end_time</th>";
+        // Loop every day,
+        // 0 = Mon.. so on and so forth
+        for ($i = 0; $i < sizeof($days); $i++) {
+            $thickness = 1;
+            $day = $days[$i];
 
-    function display($students){
-        echo 
-        "   
+            // If the current time is greater than the rows filled for the day
+            if (sizeof($day) > 0 && $time > $day_rows[$i]) {
+                // If someone is unavailable
+                // Keep looping next timeslot to see if there's more unavailability.
+                $new_time = $time + 1;
+                while (array_key_exists($new_time, $schedule) && sizeof($schedule[$new_time][$i]) > 0) {
+                    $thickness += 1;
+                    foreach ($schedule[$new_time][$i] as $cmi) {
+                        if (!in_array($cmi, $day)) {
+                            array_push($unavailable, $cmi);
+                        }
+                    }
+                    $new_time = $time + 1;
+                }
+            } else {
+                $new_time = $time + 1;
+                while (array_key_exists($new_time, $schedule) && sizeof($schedule[$new_time][$i]) == 0) {
+                    $thickness += 1;
+                }
+            }
+
+            echo "<td rowspan='$thickness'>";
+
+            if ($day != null && sizeof($day) > 0)
+                foreach ($day as $person) {
+                    echo $person;
+                }
+
+            echo "</td>";
+            $day_rows[$i] += $thickness;
+        }
+        echo "</tr>";
+    }
+    echo "<table>";
+}
+
+### START OF DO NOT MODIFY ###
+
+function display($students)
+{
+    echo
+    "   
         <!DOCTYPE html>
         <html>
             <body>
         ";
 
-        display_timetable($students);
+    display_timetable($students);
 
-        echo 
-        "
+    echo
+    "
             </body>
         </html>
         ";
-    }
+}
 
-    if (count($_GET) != 0){
-        $sids = $_GET['sids'];
-        $students = [];
-        $studentDAO = new StudentDAO();
-        foreach($sids as $sid){
-            $student = $studentDAO->getStudent($sid);
-            $students[] = $student;
-        }
-        display($students);
+if (count($_GET) != 0) {
+    $sids = $_GET['sids'];
+    $students = [];
+    $studentDAO = new StudentDAO();
+    foreach ($sids as $sid) {
+        $student = $studentDAO->getStudent($sid);
+        $students[] = $student;
     }
+    display($students);
+}
 
-    ### END OF DO NOT MODIFY ###
+### END OF DO NOT MODIFY ###
 
 ?>
